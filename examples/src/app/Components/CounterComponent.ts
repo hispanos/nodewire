b0bc38460c10fa127b04dde900e4ba9bb1eb9270
Mailer Martinez <mailermartinez@gmail.com>
2025-12-16 21:05:27 -0500

    feat: add async events

--- examples/src/app/Components/CounterComponent.ts
+++ examples/src/app/Components/CounterComponent.ts
@@ -1,28 +1,34 @@
-import { Component } from 'framework-mvc-nodewire';
+import { Component } from "framework-mvc-nodewire";
 
 export class CounterComponent extends Component {
-    public count: number = 0;
-    public view: string = 'components/counter';
+  public count: number = 0;
+  public view: string = "components/counter";
+  public loading: boolean = false;
 
-    constructor(initialValue: number = 0, id?: string) {
-        super('CounterComponent', id);
-        this.count = initialValue;
-    }
+  constructor(initialValue: number = 0, id?: string) {
+    super("CounterComponent", id);
+    this.count = initialValue;
+  }
 
-    public increment(value: number = 1): void {
-        console.log('increment', value);
-        this.count += value;
-        console.log(`[NodeWire] Contador incrementado a ${this.count}`);
-    }
+  public increment(value: number = 1): void {
+    console.log("increment", value);
+    this.count += value;
+    console.log(`[NodeWire] Contador incrementado a ${this.count}`);
+  }
 
-    public decrement(): void {
-        this.count -= 1;
-        console.log(`[NodeWire] Contador decrementado a ${this.count}`);
+  public async decrement(): Promise<void> {
+    this.loading = true;
+    try {
+      await new Promise((res) => setTimeout(res, 3000)); // tu espera
+      this.count -= 1;
+    } finally {
+      this.loading = false;
     }
+    console.log(`[NodeWire] Contador decrementado a ${this.count}`);
+  }
 
-    public reset(): void {
-        this.count = 0;
-        console.log(`[NodeWire] Contador reseteado`);
-    }
+  public reset(): void {
+    this.count = 0;
+    console.log(`[NodeWire] Contador reseteado`);
+  }
 }
-
